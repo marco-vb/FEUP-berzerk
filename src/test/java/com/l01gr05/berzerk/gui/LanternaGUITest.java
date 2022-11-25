11d9628c0a6a76ec06be96d7230bea00fae6a150
goncalo2k <goncalo.miranda.cplx@gmail.com>
2022-11-25 02:13:28 +0000

    Added tests for the LanternaGUI class

--- src/test/java/com/l01gr05/berzerk/gui/LanternaGUITest.java
+++ src/test/java/com/l01gr05/berzerk/gui/LanternaGUITest.java
@@ -2,6 +2,8 @@ package com.l01gr05.berzerk.gui;
 
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
+import com.l01gr05.berzerk.mvc.model.Position;
+import com.l01gr05.berzerk.mvc.model.elements.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -22,7 +24,44 @@ public class LanternaGUITest {
     }
 
     @Test
-    void test() {
-        // TODO
+    void drawAgent() {
+        gui.drawAgent(new Agent(new Position(1,1)));
+
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, "A");
+    }
+
+    @Test
+    void drawExit() {
+        gui.drawExit(new Exit(new Position(1,1)));
+
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, " ");
+    }
+
+    @Test
+    void drawWall() {
+        gui.drawWall(new Wall(new Position(1,1)));
+
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, "#");
+    }
+
+    @Test
+    void drawEnemy() {
+        gui.drawEnemy(new Enemy(new Position(1,1)));
+
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, "E");
+    }
+
+    //Could be better?
+    @Test
+    void drawBullet() {
+        gui.drawBullet(new Bullet(new Position(1,1), 'N'));
+
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, ".");
+    }
+
+    @Test
+    void draw() {
+        gui.draw(1,1, 'b');
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 1, "b");
     }
 }
