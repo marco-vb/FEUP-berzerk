0b136de71a4ee2fdb8fd210632d2c1fb64c2d899
marco-vb <marcofilipe03@hotmail.com>
2022-12-09 17:42:02 +0000

    Added tests to KeyViewer

--- src/test/java/com/l01gr05/berzerk/mvc/view/KeyViewerTest.java
+++ src/test/java/com/l01gr05/berzerk/mvc/view/KeyViewerTest.java
@@ -3,28 +3,30 @@ package com.l01gr05.berzerk.mvc.view;
 import com.googlecode.lanterna.screen.Screen;
 import com.l01gr05.berzerk.gui.GUI;
 import com.l01gr05.berzerk.mvc.model.Position;
+import com.l01gr05.berzerk.mvc.model.elements.Key;
 import com.l01gr05.berzerk.mvc.model.elements.Tower;
+import com.l01gr05.berzerk.mvc.view.game.KeyViewer;
 import com.l01gr05.berzerk.mvc.view.game.TowerViewer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-public class TowerViewerTest {
-    private TowerViewer towerViewer;
+public class KeyViewerTest {
+    private KeyViewer keyViewer;
     private GUI gui;
-    private Tower tower;
+    private Key key;
     private Screen screen;
 
     @BeforeEach
     void setUp()  {
-        this.tower = new Tower(new Position(10,10));
-        towerViewer = new TowerViewer(tower);
+        this.key = new Key(new Position(10,10));
+        keyViewer = new KeyViewer(key);
         gui = Mockito.mock(GUI.class);
     }
 
     @Test
     public void testDraw() {
-        towerViewer.draw(gui);
-        Mockito.verify(gui, Mockito.times(1)).drawTower(towerViewer.getModel());
+        keyViewer.draw(gui);
+        Mockito.verify(gui, Mockito.times(1)).drawKey(keyViewer.getModel());
     }
 }
