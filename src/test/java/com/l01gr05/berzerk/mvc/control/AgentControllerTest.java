0a142ebe2fb4e42d5024956716a57c379fd26f95
marco-vb <marcofilipe03@hotmail.com>
2022-12-10 21:00:18 +0000

    Added more tests to Agent Controller

--- src/test/java/com/l01gr05/berzerk/mvc/control/AgentControllerTest.java
+++ src/test/java/com/l01gr05/berzerk/mvc/control/AgentControllerTest.java
@@ -7,6 +7,8 @@ import com.l01gr05.berzerk.mvc.model.Position;
 import com.l01gr05.berzerk.mvc.model.arena.Arena;
 import com.l01gr05.berzerk.mvc.model.elements.Agent;
 import com.l01gr05.berzerk.mvc.model.elements.Exit;
+import com.l01gr05.berzerk.mvc.model.elements.Key;
+import com.l01gr05.berzerk.mvc.model.elements.Wall;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -21,14 +23,16 @@ public class AgentControllerTest {
     private Arena arena;
     private Exit exit;
     private Game game;
+    private Key key;
     @BeforeEach
     void setUp() {
         game = Mockito.mock(Game.class);
         agent = new Agent(new Position(1, 1));
         arena = new Arena(game);
-        exit = new Exit(new Position(9, 9));
+        exit = new Exit(new Position(1, 3));
         arena.addElement(agent);
         arena.addElement(exit);
+        arena.addElement(new Key(new Position(2, 1)));
         agentController = new AgentController(arena);
     }
 
@@ -54,5 +58,32 @@ public class AgentControllerTest {
     void testMoveLeft() throws IOException {
         agentController.update(game, GUI.INPUT.LEFT);
         assertEquals(new Position(0, 1), agent.getPosition());
+    }
+
+    @Test
+    void testShoot() throws IOException {
+        agentController.update(game, GUI.INPUT.SHOOT);
+        assertEquals(1, arena.getBullets().size());
+    }
+
+    @Test
+    void moveUpWithWall() throws IOException {
+        arena.addElement(new Wall(new Position(1, 0)));
+        agentController.update(game, GUI.INPUT.UP);
+        assertEquals(agent.getPosition(), agent.getInitialPosition());
+    }
+
+    @Test
+    void moveToExit() throws IOException {
+        agentController.update(game, GUI.INPUT.DOWN);
+        agentController.update(game, GUI.INPUT.DOWN);
+        Mockito.verify(game, Mockito.times(1)).nextLevel();
+    }
+
+    @Test
+    void moveToExitWithWall() throws IOException {
+        arena.addElement(new Wall(new Position(1, 2)));
+        agentController.update(game, GUI.INPUT.DOWN);
+        assertEquals(agent.getPosition(), agent.getInitialPosition());
     }
 }
